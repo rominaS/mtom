4eb668b123eb6ff600d8e60574786987171a9e02
rominaS <romina.saravi@icloud.com>
2020-02-13 13:35:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,10 @@
     });
 
     api.onError(function (err) {
-        var error_box = document.querySelector('#error_box');
-        error_box.innerHTML = err;
-        error_box.style.visibility = 'visible';
+        //var error_box = document.querySelector('#error_box');
+        //error_box.innerHTML = err;
+        alert(err);
+        //error_box.style.visibility = 'visible';
     });
 
     api.onUserUpdate(function (username) {
@@ -23,21 +24,33 @@
     });
 
     api.onMessageUpdate(function (messages) {
+        var message_list = ["#messages", "#messages2", "#messages3", "#messages4", "#messages5", "#messages6", "#messages7", "#messages8"];
+        //message_List.forEach(element => document.querySelector(element).innerHTML = '');
         document.querySelector('#messages').innerHTML = '';
+        document.querySelector('#messages2').innerHTML = '';
+        document.querySelector('#messages3').innerHTML = '';
+        document.querySelector('#messages4').innerHTML = '';
+        document.querySelector('#messages5').innerHTML = '';
+        document.querySelector('#messages6').innerHTML = '';
+        document.querySelector('#messages7').innerHTML = '';
+        document.querySelector('#messages8').innerHTML = '';
+
+        var i = 0;
         messages.forEach(function (message) {
+
             var elmt = document.createElement('div');
             elmt.className = "message";
             elmt.id = "msg" + message._id;// <img class="message_picture" src="media/user.png" alt="${message.username}">
             elmt.innerHTML = `
-                <div class="message_user">
-                </div>
-                <div class="message_username">${message.username}</div>
-                
-                <div class="message_content">${message.content}</div>
-                <div class="upvote-icon icon">${message.upvote}</div>
-                <div class="downvote-icon icon">${message.downvote}</div>
-                <div class="delete-icon icon"></div>
+            <div class="message_user">
+            </div>
+            <div class="message_username">${message.username}</div>
+            <div class="message_content">${message.content}</div>
+            <div class="upvote-icon icon">${message.upvote}</div>
+            <div class="downvote-icon icon">${message.downvote}</div>
+            <div class="delete-icon icon"></div>
             `;
+
             elmt.querySelector(".delete-icon").addEventListener('click', function () {
                 api.deleteMessage(message._id);
             });
@@ -47,7 +60,10 @@
             elmt.querySelector(".downvote-icon").addEventListener('click', function () {
                 api.downvoteMessage(message._id);
             });
-            document.querySelector("#messages").prepend(elmt);
+            //alert("end of ele create");
+            document.querySelector(message_list[i]).prepend(elmt);
+            i += 1;
+
         });
     });
 
@@ -59,5 +75,5 @@
             api.addMessage(content);
         });
     });
-}())
+}());
 
